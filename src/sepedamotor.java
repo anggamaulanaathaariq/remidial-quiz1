8ea2b54112aa1f9678dbfe25f2dae8ff5b1a2498
Angga Maulana A <anggamaulanaathaariq@anggamaulanaathaariq>
2018-11-04 16:00:55 +0700

    Class Transaction Edit

--- src/sepedamotor.java
+++ src/sepedamotor.java
@@ -9,10 +9,38 @@ public class sepedamotor {
     double km, bensin;
     orang pemilik;
    
-    public void kecepatan (int k) {
+    public void set_kecepatan (int k) {
         kecepatan = k;
     }
     public void set_km (double k ){
-        
+        km = k;        
     }
+    public void set_bensin (double b) {
+        bensin = (b/6500);
+    }
+    public void set_pemilik (orang o) {
+        pemilik = o;
+    }
+    int get_kecepatan(){
+        return kecepatan;
+    }
+    double get_km(){
+        return km;
+    }
+    double get_bensin(){
+        return bensin;
+    }
+    orang get_pemilik() {
+        return pemilik;
+    }
+    String get_jenis(){
+        return jenis;
+    }
+    
+    void  jalan (){
+        for(;;)
+            System.out.println("");
+    }
+        
 }
+
