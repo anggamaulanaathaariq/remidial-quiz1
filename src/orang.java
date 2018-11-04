2bcab44bf5d9c9368ae24cd3750e6c22f3ec9c4f
Angga Maulana A <anggamaulanaathaariq@anggamaulanaathaariq>
2018-11-04 17:03:43 +0700

    Class Transaction Edit

--- src/orang.java
+++ src/orang.java
@@ -22,5 +22,16 @@ public class orang {
         sepedamtr = new sepedamotor[10];
         System.out.println("Selamat Datang, "+ nm);
     }
-    void beli_motor
+    void beli_motor(long harga, sepedamotor spd){
+        sepedamtr[jumlah] = spd;
+        jumlah++;
+        System.out.println(nama+" Membeli sepeda motor "+ spd.get_jenis()+" seharga "+harga);
+    }
+    void daftar_motor(){
+        System.out.println("Motor yang "+nama+ " :");
+        for(int i=0; i<jumlah; i++)
+        {
+            System.out.println(sepedamtr[i].get_jenis());
+        }
+    }
 }
