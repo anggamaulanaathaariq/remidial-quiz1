78f5e05ab1267bc58d5c8c98a905e7e515168e6f
Angga Maulana A <anggamaulanaathaariq@anggamaulanaathaariq>
2018-11-04 16:20:37 +0700

    Class Transaction Edit

--- src/orang.java
+++ src/orang.java
@@ -6,10 +6,21 @@ public class orang {
     String ktp;
     String nama;
     String alamat;
-    int kecepatan;
-    double km, bensin;
-    orang pemilik;
+    sepedamotor [] sepedamtr;
+    int jumlah;
     
-   
+    String getnama() {
+        return nama;
+    }
     
+    public orang (String kt, String nm, String almt){
+        ktp = kt;
+        nama = nm;
+        alamat = almt;
+        jumlah = 0;
+        
+        sepedamtr = new sepedamotor[10];
+        System.out.println("Selamat Datang, "+ nm);
+    }
+    void beli_motor
 }
